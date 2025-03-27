ec9b84b0ebc54c778149c4bbd5f09fd4c4ec2fb2
Mitchell Durand <mitchelldurandb25@gmail.com>
2025-03-26 23:33:53 -0500

    agregando variables de entorno a DBCONECTION

--- database/connectiondb.js
+++ database/connectiondb.js
@@ -1,15 +1,17 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'clientestigo2',
-  //database: 'tigo_dbclientes2',
-  password: 'Ingenieria5704722',
-  port: 5432, // Por defecto, pero puedes modificarlo si es necesario
-  max: 100, // Número máximo de conexiones en el pool
-  idleTimeoutMillis: 30000, // Tiempo de inactividad antes de cerrar una conexión
-  connectionTimeoutMillis: 3000 // Tiempo máximo para establecer una conexión
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'clientestigo2',
+  password: process.env.DB_PASS || 'Ingenieria5704722',
+  port: process.env.DB_PORT || 5432, // Por defecto, pero puedes modificarlo si es necesario
+  // max: 100, // Número máximo de conexiones en el pool
+  // idleTimeoutMillis: 30000, // Tiempo de inactividad antes de cerrar una conexión
+  // connectionTimeoutMillis: 3000 // Tiempo máximo para establecer una conexión
 });
 
 module.exports = pool;
